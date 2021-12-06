821b91abc06530bb24c206dd0ef9d7a4aa92cebf
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2021-12-06 10:21:55 +0000

    LanternFishCounts: Simplify since part 1 and 2 are only tracking counts
    We can just use a long[] instead.

--- src/main/java/com/gordonreid/adventofcode2021/december06/LanternFishCounts.java
+++ src/main/java/com/gordonreid/adventofcode2021/december06/LanternFishCounts.java
@@ -2,37 +2,32 @@ package com.gordonreid.adventofcode2021.december06;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class LanternFishCounts {
 
     private static final int INITIAL_TIMER = 8;
     private static final int TIMER_AFTER_GIVING_BIRTH = 6;
 
-    private final List<LanternFishCount> lanternFishCounts = IntStream.rangeClosed(0, INITIAL_TIMER)
-            .mapToObj(LanternFishCount::new)
-            .toList();
+    private final long[] lanternFishCounts = new long[INITIAL_TIMER + 1];
 
     public LanternFishCounts(List<String> input) {
         List<Integer> inputTimers = Arrays.stream(input.get(0).split(","))
                 .map(Integer::parseInt)
                 .toList();
-        inputTimers.forEach(timer -> lanternFishCounts.get(timer).incrementCount());
+        inputTimers.forEach(timer -> lanternFishCounts[timer]++);
     }
 
     public long totalCount() {
-        return lanternFishCounts.stream().map(LanternFishCount::getCount).reduce(0L, Long::sum);
+        return Arrays.stream(lanternFishCounts).sum();
     }
 
     public void nextDay() {
-        long newFish = lanternFishCounts.get(0).getCount();
-        // Decrement counter for all existing fish with a non-zero timer
-        for (int i = 0; i < lanternFishCounts.size() - 1; i++) {
-            lanternFishCounts.get(i).setCount(lanternFishCounts.get(i + 1).getCount());
-        }
-        // Fish that have given birth is the same number as new fish created. Those have a timer of 6 so we _increase_
-        // the count of that timer by the number of fish that have given birth
-        lanternFishCounts.get(TIMER_AFTER_GIVING_BIRTH).increaseCount(newFish);
-        lanternFishCounts.get(INITIAL_TIMER).setCount(newFish);
+        long newFish = lanternFishCounts[0];
+        // Decrement counter for all existing fish with a non-zero timer by copying position [1..end] to [0..end-1]
+        System.arraycopy(lanternFishCounts, 1, lanternFishCounts, 0, lanternFishCounts.length - 1);
+        // Fish that have given birth is the same number as new fish created. The existing fish have a timer of 6 for
+        // when they'll next give birth so we _increase_ the count of that timer by the number of fish that have given birth
+        lanternFishCounts[TIMER_AFTER_GIVING_BIRTH] += newFish;
+        lanternFishCounts[INITIAL_TIMER] = newFish;
     }
 }
