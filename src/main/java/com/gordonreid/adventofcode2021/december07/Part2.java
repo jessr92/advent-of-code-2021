1c10d8b32ed2a922fe964be966767e671437ece8
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2021-12-07 09:39:01 +0000

    Day 7 Part2: Typo fix

--- src/main/java/com/gordonreid/adventofcode2021/december07/Part2.java
+++ src/main/java/com/gordonreid/adventofcode2021/december07/Part2.java
@@ -10,13 +10,13 @@ public class Part2 {
         return FuelCostCalculator.run(positions, Part2::fuelCostForMove);
     }
 
-    private static int fuelCostForMove(int desiredPosition, int currentPositions) {
+    private static int fuelCostForMove(int desiredPosition, int currentPosition) {
         // 1 unit of fuel for first position move
         // 2 units of fuel for second position move
         // Linearly increasing from there.
         // Fuel cost is this sum(1...n) where n is the number of position changes
         // This can be calculated by using the formula 1/2 n(n+1).
-        int positionChanges = Math.abs(desiredPosition - currentPositions);
+        int positionChanges = Math.abs(desiredPosition - currentPosition);
         return (positionChanges * (positionChanges + 1)) / 2;
     }
 }
