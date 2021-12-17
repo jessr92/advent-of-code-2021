0bd907a25bf7715b4b0db3bfdbbf5a656309f970
Gordon Reid <gordon.reid1992@hotmail.co.uk>
2021-12-17 18:46:24 +0000

    Coordinates: Add helpers to update Coordinates when moving forward or up

--- src/main/java/com/gordonreid/adventofcode2021/helpers/GridHelpers.java
+++ src/main/java/com/gordonreid/adventofcode2021/helpers/GridHelpers.java
@@ -5,5 +5,13 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class GridHelpers {
     public record Coordinates(int x, int y) {
+
+        public Coordinates moveForward(int forward) {
+            return new Coordinates(x + forward, y);
+        }
+
+        public Coordinates moveUp(int up) {
+            return new Coordinates(x, y + up);
+        }
     }
 }
